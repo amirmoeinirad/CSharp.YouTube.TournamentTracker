5dd59e54a7f1a34d5979a9f00527eb26c202ce70
Amir Moeini Rad <moeini@shabakehafzar.com>
2025-11-06 13:57:28 +0330

    Add project files.

---
+++ TrackerLibrary/PersonModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TrackerLibrary
+{
+    // This class holds the data for a person.
+    public class PersonModel
+    {
+        // We are initializing the properties to avoid null reference issues.
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string EmailAddress { get; set; } = string.Empty;
+        public string CellPhoneNumber { get; set; } = string.Empty;
+    }
+}
---
+++ TrackerLibrary/TeamModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TrackerLibrary
+{
+    // This class hold the data for a team.
+    public class TeamModel
+    {
+        // Here, we are initializing the properties to avoid null reference issues.
+        public List<PersonModel> TeamMembers { get; set; } = [];
+        public string TeamName { get; set; } = string.Empty;
+    }
+}
